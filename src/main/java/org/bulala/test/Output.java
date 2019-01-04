f5ce4e225eb454adda011766c2556baad83e32ff
bulala <you@example.com>
2019-01-04 13:25:20 +0800

    new Test

---
+++ src/main/java/org/bulala/test/Output.java
@@ -0,0 +1,12 @@
+package org.bulala.test;
+
+import java.util.Scanner;
+
+public class Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("one number");
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
